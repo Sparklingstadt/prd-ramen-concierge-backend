8e28baf8012dfff84ff0392825c81d7cae650837
Sparklingstadt <sparklingstadt@outlook.jp>
2026-05-15 08:24:18 +0900

    feat(seeds.rb): add initial RamenLog entry

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,4 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+RamenLog.create!(soup_type: "しょうゆ", topping: "チャーシュー", noodle_size: "たくさん")
